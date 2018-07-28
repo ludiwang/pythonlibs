8bfe3890786a40c1b10d595937fd6adfb6c191ea
ludiwang <ludiwang@yahoo.com>
2018-07-28 10:20:35 +0200

    Modified again names of procedures and variables acoording to PEP8.

--- menu.py
+++ menu.py
@@ -1,29 +1,29 @@
 class Menu:
     def __init__(self, menu_entry: list):
-        self.menu_list = []
-        self.menu_list.append(menu_entry)
-        self.cur_cmd = ""
-        self.menu_name_hist = []
-        self.menu_name_hist.append(menu_entry[0])
+        self._menu_list = []
+        self._menu_list.append(menu_entry)
+        self._cur_cmd = ""
+        self._menu_name_hist = []
+        self._menu_name_hist.append(menu_entry[0])
 
-    def level_up(self):
-        if len(self.menu_name_hist) == 1:
+    def _level_up(self):
+        if len(self._menu_name_hist) == 1:
             print("Already at Top level.")
             self.show_menu()
-        curLevel = self.get_menu_entry(self.menu_name_hist[-1])[2]
-        while int(self.get_menu_entry(self.menu_name_hist[-1])[2]) >= int(curLevel):
-            self.menu_name_hist.pop()
+        curLevel = self._get_menu_entry(self._menu_name_hist[-1])[2]
+        while int(self._get_menu_entry(self._menu_name_hist[-1])[2]) >= int(curLevel):
+            self._menu_name_hist.pop()
         self.show_menu()
 
     def get_choice(self):
-        return self.cur_cmd
+        return self._cur_cmd
 
     def add_menu_entry(self, menu_entry: list):
-        self.menu_list.append(menu_entry)
+        self._menu_list.append(menu_entry)
 
-    def get_menu_entry(self, name):
+    def _get_menu_entry(self, name):
         menu_entry = []
-        for entry in self.menu_list:
+        for entry in self._menu_list:
             if entry[0] == name:
                 menu_entry = entry
         if len(menu_entry) == 0:
@@ -31,8 +31,8 @@ class Menu:
             exit()
         return menu_entry
 
-    def show_menu_entry(self, name):
-        menu_entry = self.get_menu_entry(name)
+    def _show_menu_entry(self, name):
+        menu_entry = self._get_menu_entry(name)
         print("{}{}{}".format(30*'=', menu_entry[1], 30*'='))
         for ind, val in enumerate(menu_entry[3]):
             # print("{}.  {} {}".format(ind+1 , val[:val.find(":")], val[val.find(":")+1:]))
@@ -41,10 +41,10 @@ class Menu:
         return menu_entry
 
     def show_menu(self):
-        self.run_menu_entry(self.menu_name_hist[-1])
+        self._run_menu_entry(self._menu_name_hist[-1])
 
-    def run_menu_entry(self, name):
-        menu_entry = self.show_menu_entry(name)
+    def _run_menu_entry(self, name):
+        menu_entry = self._show_menu_entry(name)
         choice = input("Please choose option: ").strip()
         for ind, val in enumerate(menu_entry[3]):
             if ind + 1 == int(choice):
@@ -54,10 +54,10 @@ class Menu:
                     if menu_item_name == "exit":
                         exit()
                     if menu_item_name == "up":
-                        self.level_up()
+                        self._level_up()
                     else:
-                        self.menu_name_hist.append(menu_item_name)
-                        self.run_menu_entry(menu_item_name)
+                        self._menu_name_hist.append(menu_item_name)
+                        self._run_menu_entry(menu_item_name)
                 else:
-                    self.cur_cmd = cmd[:cmd.find(":")]
+                    self._cur_cmd = cmd[:cmd.find(":")]
                 break
